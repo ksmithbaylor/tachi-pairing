472d6fe223bc82ca3731fc50c1e13b896a792208
Evan Sparkman <me@evansparkman.com>
2017-01-27 09:51:55 -0500

    Update background color for my name

--- src/store/devs.js
+++ src/store/devs.js
@@ -2,7 +2,7 @@ export default [
   { "color": "#B2EBF2", "name": "Chuck" },
   { "color": "#F8BBD0", "name": "Dave" },
   { "color": "#D1C4E9", "name": "Eric" },
-  { "color": "#C8E6C9", "name": "Evan" },
+  { "color": "#0033A0", "name": "Evan" },
   { "color": "#FFCDD2", "name": "Garey" },
   { "color": "#D7CCC8", "name": "John" },
   { "color": "#B2DFDB", "name": "Julie" },
