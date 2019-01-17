7285f6471f46590b0cac669097861a29843451f4
Kevin Smith <ksmithbaylor@gmail.com>
2019-01-17 09:33:45 -0500

    Put in Tachi dev names

--- src/store/devs.js
+++ src/store/devs.js
@@ -1,10 +1,10 @@
 export default [
-  { "color": "#B2EBF2", "name": "Chuck" },
-  { "color": "#F8BBD0", "name": "Dave" },
-  { "color": "#D1C4E9", "name": "Eric" },
-  { "color": "#0033A0", "name": "Evan" },
-  { "color": "#FFCDD2", "name": "Garey" },
-  { "color": "#D7CCC8", "name": "John" },
-  { "color": "#BBDEFB", "name": "Kevin" },
-  { "color": "#FFE0B2", "name": "Kiana" }
+  { 'name': 'Aaron', 'color': 'white' },
+  { 'name': 'Brandon', 'color': 'white' },
+  { 'name': 'Elisabeth', 'color': 'white' },
+  { 'name': 'Garey', 'color': 'white' },
+  { 'name': 'Gemille', 'color': 'white' },
+  { 'name': 'Kevin', 'color': 'white' },
+  { 'name': 'Ryan', 'color': 'white' },
+  { 'name': 'Sunny', 'color': 'white' }
 ];
