5da48cf84bfd1c6afffbee1fecb74af1c991f6f3
Kevin Smith <ksmithbaylor@gmail.com>
2019-01-17 10:42:36 -0500

    Add colors for Aaron and Elisabeth

--- src/store/devs.js
+++ src/store/devs.js
@@ -1,7 +1,7 @@
 export default [
-  { 'name': 'Aaron', 'color': 'white' },
+  { 'name': 'Aaron', 'color': 'rgb(244, 223, 66)' },
   { 'name': 'Brandon', 'color': 'white' },
-  { 'name': 'Elisabeth', 'color': 'white' },
+  { 'name': 'Elisabeth', 'color': '#32A470' },
   { 'name': 'Garey', 'color': 'white' },
   { 'name': 'Gemille', 'color': 'white' },
   { 'name': 'Kevin', 'color': 'white' },
